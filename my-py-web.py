1da339e304338a1db247bbcc059b27d2a38733a7
Andre van der Lingen <andre.van.der.lingen@rivm.nl>
2024-07-14 15:44:49 +0200

    add flask port etc.

--- my-py-web.py
+++ my-py-web.py
@@ -1,9 +1,7 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
-
-if __name__ == "__main__":
-    app.run()
 
 # Defining the home page of our site
 @app.route("/")  # this sets the route to this page
@@ -14,3 +12,9 @@ def home():
 def user(name):
     return f"Hello {name}!"
 
+
+if __name__ == "__main__":
+    port = os.environ.get('FLASK_PORT') or 8080
+    port = int(port)
+
+    app.run(port=port,host='0.0.0.0')
