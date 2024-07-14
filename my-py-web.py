0c836548d8225c43817351d546ec2c3096dd3988
lingenavd <31818733+lingenavd@users.noreply.github.com>
2024-07-14 15:13:21 +0200

    Add files via upload

---
+++ my-py-web.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run()
+
+# Defining the home page of our site
+@app.route("/")  # this sets the route to this page
+def home():
+	return "Hello! this is the main page <h1>HELLO</h1>"  # some basic inline html
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
